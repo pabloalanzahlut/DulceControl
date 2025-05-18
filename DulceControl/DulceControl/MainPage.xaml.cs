be833d479e17df8a584314995bf67b2ec440b448
pabloalanzahlut <zahlut87@yahoo.com.ar>
2025-05-18 23:32:13 +0200

    primer push

---
+++ DulceControl/DulceControl/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using PastelitosApp.Models;
+using PastelitosApp.Services;
+using System.Collections.ObjectModel;
+
+namespace PastelitosApp;
+
+public partial class MainPage : ContentPage
+{
+    private ObservableCollection<Pedido> _pedidos;
+
+    public MainPage()
+    {
+        InitializeComponent();
+        _pedidos = new ObservableCollection<Pedido>();
+        PedidosCollectionView.ItemsSource = _pedidos;
+
+        CargarPedidos();
+    }
+
+    private void CargarPedidos()
+    {
+        var pedidos = PedidoService.CargarPedidos();
+
+        _pedidos.Clear();
+        foreach (var pedido in pedidos)
+        {
+            _pedidos.Add(pedido);
+        }
+    }
+}
---
+++ DulceControl/DulceControl/Services/PedidoService.cs
@@ -0,0 +1,41 @@
+﻿using PastelitosApp.Models;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace PastelitosApp.Services;
+
+public static class PedidoService
+{
+    private static string FilePath =>
+        Path.Combine(FileSystem.AppDataDirectory, "pedidos.bin");
+
+    public static List<Pedido> CargarPedidos()
+    {
+        if (!File.Exists(FilePath))
+            return new List<Pedido>();
+
+        try
+        {
+            using var fs = new FileStream(FilePath, FileMode.Open);
+            var formatter = new BinaryFormatter();
+            return (List<Pedido>)formatter.Deserialize(fs);
+        }
+        catch
+        {
+            return new List<Pedido>();
+        }
+    }
+
+    public static void GuardarPedidos(List<Pedido> pedidos)
+    {
+        try
+        {
+            using var fs = new FileStream(FilePath, FileMode.Create);
+            var formatter = new BinaryFormatter();
+            formatter.Serialize(fs, pedidos);
+        }
+        catch
+        {
+            // Error al guardar
+        }
+    }
+}
